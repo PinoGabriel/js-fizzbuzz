550b1dc12c7af8c4444a1a7b725e0ee323d7733f
PinoGabriel <pinogabriel01@gmail.com>
2023-10-30 16:10:13 +0100

    add createElement

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
+let cards = document.getElementById("my_row");
+
+
+
 
 for (let x = 1; x <= 100; x++) {
 
     if (x % 3 == 0 && x % 5 == 0) {
         console.log("FizzBuzz")
+        const m35 = document.createElement("p")
+        m35.innerHTML = `FizzBuzz`
+        cards.append(m35)
     } else if (x % 3 == 0) {
         console.log("Fizz")
+        const m3 = document.createElement("p")
+        m3.innerHTML = `Fizz`
+        cards.append(m3)
     } else if (x % 5 == 0) {
         console.log("Buzz")
+        const m5 = document.createElement("p")
+        m5.innerHTML = `Buzz`
+        cards.append(m5)
     } else {
         console.log(x)
+        const m = document.createElement("p")
+        m.innerHTML = `${x}`
+        cards.append(m)
     }
 
 }
