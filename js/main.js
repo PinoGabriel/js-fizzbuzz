6e8946f2a3c654cec58923129f5aaedf47697101
PinoGabriel <pinogabriel01@gmail.com>
2023-10-30 15:23:48 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+for (let x = 0; x <= 100; x++) {
+    if (x == 3) {
+        console.log("Fizz")
+    } else if (x == 5) {
+        console.log("Buzz")
+    } else {
+        console.log(x);
+    }
+}
+
+console.log("completo");
